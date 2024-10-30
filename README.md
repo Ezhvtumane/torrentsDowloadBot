b49e7ea3c28564099323d73413e3801388b5a083
Georgy Orlov <orlov.g.s@outlook.com>
2024-10-30 13:22:31 +0300

    commit as is

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# torrentsDowloadBot
+# torrentsDownloadBot
 
+Things todo:
+
+1) Put secrets inside image or start container with var secrets
+   ~~2) Make mandatory volume for files +
+3) download torrents with more than 1 file or dir~~
+4) Add DB fo r users and torrents and logging
+5) Auto build and docker image build (ci/cd)
+6) make different threads for clients. only one download for client.
+7) add stop command, think how to differ downloads
+8) remain downloads after container restart (DB)
+9) settings for del or not files from disk
+10) settings for zip multi files or just send as is
+11) settings for max torrent size
--- src/main/java/com/georgyorlov/torrentdownloader/command/DownloadCommand.java
+++ src/main/java/com/georgyorlov/torrentdownloader/command/DownloadCommand.java
@@ -1,15 +1,24 @@
 package com.georgyorlov.torrentdownloader.command;
 
+import static java.lang.String.format;
+
 import bt.Bt;
 import bt.data.Storage;
 import bt.data.file.FileSystemStorage;
 import bt.dht.DHTConfig;
 import bt.dht.DHTModule;
+import bt.metainfo.Torrent;
 import bt.metainfo.TorrentFile;
 import bt.runtime.BtClient;
 import bt.runtime.BtRuntime;
 import bt.runtime.BtRuntimeBuilder;
 import bt.runtime.Config;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -19,16 +28,25 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
-import static java.lang.String.format;
-
 public class DownloadCommand extends BotCommand {
 
     private static final long MAX_SIZE = 3l * 1024 * 1024 * 1024;
+    private final ConcurrentMap<String, BtClient> clients = new ConcurrentHashMap();
+    private final BtRuntime runtime = new BtRuntimeBuilder()
+        .config(new Config() {
+            @Override
+            public int getNumOfHashingThreads() {
+                return 8;//Runtime.getRuntime().availableProcessors();
+            }
+        })
+        .module(new DHTModule(new DHTConfig() {
+            @Override
+            public boolean shouldUseRouterBootstrap() {
+                return true;
+            }
+        }))
+        .autoLoadModules()
+        .build();
 
     public DownloadCommand() {
         super("download", "command for download by magnet uri");
@@ -39,85 +57,19 @@ public class DownloadCommand extends BotCommand {
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
         //strings -> arguments with magnet link
         if (strings.length == 1) { //wait only one magnet link
-            Path targetDirectory = Paths.get("");
+            String magnetUrl = strings[0];
+            Path targetDirectory = Paths.get("./files");
             Storage storage = new FileSystemStorage(targetDirectory);
-            BtRuntime runtime = new BtRuntimeBuilder()
-                    .config(new Config() {
-                        @Override
-                        public int getNumOfHashingThreads() {
-                            return Runtime.getRuntime().availableProcessors();
-                        }
-                    })
-                    .autoLoadModules()
-                    .module(new DHTModule(new DHTConfig() {
-                        @Override
-                        public boolean shouldUseRouterBootstrap() {
-                            return true;
-                        }
-                    }))
-                    .build();
 
             BtClient client = Bt.client(runtime)
-                    .storage(storage)
-                    .magnet(strings[0])
-                    .afterTorrentFetched(torrent -> {
-                        if (torrent.getSize() > MAX_SIZE) { //more that 3GB
-                            try {
-                                absSender.execute(new SendMessage() {{
-                                    setChatId(chat.getId());
-                                    setText("Error. File is too big. No more that 3GB allowed. Stop downloading.");
-                                }});
-                            } catch (TelegramApiException ex) {
-                                //todo
-                            }
-                            throw new IllegalStateException("Error. File is too big. No more that 3GB allowed. Stop downloading.");
-                        }
-                        if (torrent.getFiles().size() > 1) {
-                            try {
-                                absSender.execute(new SendMessage() {{
-                                    setChatId(chat.getId());
-                                    setText("Error. More than 1 file in torrent.");
-                                }});
-                            } catch (TelegramApiException ex) {
-                                //todo
-                            }
-                            //how to stop download?
-                            //client.stop();
-                            throw new IllegalStateException("Error. More than 1 file in torrent.");
-                        }
-                    })
-                    .afterDownloaded(torrent -> {
-                        List<TorrentFile> files = torrent.getFiles();
-                        //todo: if not one file -> zip all files and send
-                        if (files.size() == 1) {
-                            files.forEach(torrentFile -> {
-                                List<String> pathElements = torrentFile.getPathElements();
-                                if (pathElements.size() == 1) {
-                                    File file = new File(pathElements.stream().findFirst().get());
-                                    InputFile inputFile = new InputFile();
-                                    inputFile.setMedia(file);
-                                    SendDocument sendDocument = new SendDocument();
-                                    sendDocument.setChatId(chat.getId());
-                                    sendDocument.setDocument(inputFile);
+                .storage(storage)
+                .magnet(magnetUrl)
+                .afterTorrentFetched(torrent -> afterFetched(absSender, chat, torrent, magnetUrl))
+                .afterDownloaded(torrent -> afterDownload(absSender, chat, torrent))
+                .stopWhenDownloaded()
+                .build();
 
-                                    try {
-                                        absSender.execute(sendDocument);
-                                    } catch (TelegramApiException exception) {
-                                        //todo
-                                        file.delete();
-                                    }
-                                    file.delete();
-                                } else {
-                                    String fullPath = pathElements.stream().reduce((s, s2) -> s + " -> " + s2).get();
-                                    throw new IllegalStateException(format("Subdirectories is not allowed %s", fullPath));
-                                }
-                            });
-                        } else {
-                            throw new IllegalStateException("More than 1 file in torrent. Stop.");
-                        }
-                    })
-                    .stopWhenDownloaded()
-                    .build();
+            clients.put(magnetUrl, client);
 
             client.startAsync(state -> {
                 boolean complete = (state.getPiecesRemaining() == 0);
@@ -129,11 +81,80 @@ public class DownloadCommand extends BotCommand {
                     client.stop();
                     //}
                 }
-                System.out.println(format("Remain %s, Downloaded %s. Peers connected %s", state.getPiecesRemaining(), state.getDownloaded(), state.getConnectedPeers().stream().map(k -> k.getPeer().getInetAddress().getHostAddress()).reduce("", (s, inetAddress) -> s + " " + inetAddress)));
-            }, 1000).join();
+                System.out.println(format("This: %s, Remain %s, Downloaded %s. Peers connected %s",
+                                          client.toString(),
+                                          state.getPiecesRemaining(),
+                                          state.getDownloaded(),
+                                          state.getConnectedPeers().stream().map(k -> k.getPeer().getInetAddress().getHostAddress()).reduce("", (s, inetAddress) -> s + " " + inetAddress)));
+            }, 5000);
+
         } else {
             throw new RuntimeException("More than 1 argument. Stop.");
         }
 
+    }
+
+    private void afterDownload(AbsSender absSender, Chat chat, Torrent torrent) {
+        //telegram, если кидать в него папку - делает zip cо всем содержимым папки
+        final List<TorrentFile> files = torrent.getFiles();
+        files.forEach(torrentFile -> {
+            final List<String> pathElements = torrentFile.getPathElements();
+            File file = new File("./files/"
+                                     + torrent.getName()
+                                     + "/"
+                                     + pathElements.stream().reduce((s, s1) -> s + "/" + s1).orElse(""));
+            InputFile inputFile = new InputFile();
+            inputFile.setMedia(file);
+            SendDocument sendDocument = new SendDocument();
+            sendDocument.setChatId(chat.getId());
+            sendDocument.setDocument(inputFile);
+
+            try {
+                absSender.execute(sendDocument);
+            } catch (TelegramApiException exception) {
+                throw new RuntimeException(exception);
+            } finally {
+                file.delete();
+            }
+        });
+
+    }
+
+
+    private void afterFetched(AbsSender absSender, Chat chat, Torrent torrent, String magnetUrl) {
+        if (torrent.getSize() > MAX_SIZE) { //more that 3GB -> toggle
+            try {
+                absSender.execute(new SendMessage() {{
+                    setChatId(chat.getId());
+                    setText("Error. File is too big. No more that 3GB allowed. Stop downloading.");
+                }});
+            } catch (TelegramApiException ex) {
+                throw new RuntimeException(ex);
+            }
+            BtClient btClient = clients.get(magnetUrl);
+            btClient.stop();
+            SendMessage message = new SendMessage();
+            message.setText("Error. File is too big. No more that 3GB allowed. Stop downloading.");
+            message.setChatId(chat.getId());
+            try {
+                absSender.execute(message);
+            } catch (TelegramApiException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new IllegalStateException("Error. File is too big. No more that 3GB allowed. Stop downloading.");
+        }
+        /*if (torrent.getFiles().size() > 1) {
+            BtClient btClient = clients.get(magnetUrl);
+            btClient.stop();
+            SendMessage message = new SendMessage();
+            message.setText("Error. More than 1 file in torrent.");
+            message.setChatId(chat.getId());
+            try {
+                absSender.execute(message);
+            } catch (TelegramApiException ex) {
+                //todo
+            }
+            throw new IllegalStateException("Error. More than 1 file in torrent.");
+        }*/
     }
 }
